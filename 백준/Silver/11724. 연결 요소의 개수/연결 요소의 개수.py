4ade45e893c8241994f6c2b9b7df7ecdf0c6ce0e
yumiKKKK <121678395+yumiKKKK@users.noreply.github.com>
2024-05-09 18:35:26 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 724 ms, Memory: 64980 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,29 @@
+from collections import deque
+import sys
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [[] for i in range(n+1)]
+for i in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+    
+def bfs(graph, x, visited):
+    queue = deque([x])
+    visited[x] = True
+    while queue:
+        x = queue.popleft()
+        for i in graph[x]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+            
+visited = [False] * (n+1) 
+count = 0
+for i in range(1, n+1):
+    if not visited[i]:
+        bfs(graph, i, visited)
+        count+=1
+print(count)
