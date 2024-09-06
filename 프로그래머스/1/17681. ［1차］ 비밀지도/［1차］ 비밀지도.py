d387e0261900a4f2a7909a0399862ac83ae0c3d7
yumiKKKK <121678395+yumiKKKK@users.noreply.github.com>
2024-09-06 14:04:46 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,10 @@
+def solution(n, arr1, arr2):
+    answer = []
+    
+    for i in range(n):
+        tmp = bin(arr1[i] | arr2[i])
+        tmp = tmp[2:].zfill(n)
+        tmp = tmp.replace('1','#').replace('0',' ')
+        answer.append(tmp)
+    
+    return answer
